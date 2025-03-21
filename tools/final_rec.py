ed8e79655d8a715f471490ab4d66318c0d46cfa1
Francesco Gervino <107546446+FrancescoGervino13@users.noreply.github.com>
2025-03-21 10:33:09 +0100

    Update final_rec.py

--- tools/final_rec.py
+++ tools/final_rec.py
@@ -12,8 +12,8 @@ import json
 
 import logging
 
-AZURE_API_KEY="94ec2d76955a48488952c81f0d591e94"
-AZURE_ENDPOINT="https://iitlines-swecentral1.openai.azure.com/"
+AZURE_API_KEY=""
+AZURE_ENDPOINT=""
 
 class TextInImage:
     def __init__(self):
